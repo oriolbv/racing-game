9d70aae3bd51389d9d3878ac7b98f5d3ba13374d
Oriol Burgaya <oriolbv@gmail.com>
2020-09-25 22:52:14 +0200

    GameplayManager added

---
+++ Assets/Scripts/GameplayManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityStandardAssets.Vehicles.Car;
+
+public class GameplayManager : MonoBehaviour
+{
+    private CarController m_CarController; // Reference to car we are controlling
+
+    
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
