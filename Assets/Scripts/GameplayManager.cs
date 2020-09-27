b10738a72e40845575aff3c3602161b0a11c7c4d
Oriol Burgaya <oriolbv@gmail.com>
2020-09-27 19:34:15 +0200

    Countdown implemented

--- Assets/Scripts/GameplayManager.cs
+++ Assets/Scripts/GameplayManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -9,17 +10,43 @@ public class GameplayManager : MonoBehaviour
     public Text CountdownText;
 
 
-    private CarController m_CarController; // Reference to car we are controlling
+    public CarController m_CarController; // Reference to car we are controlling
 
     
     void Start()
     {
-        
+        m_CarController.FullTorqueOverAllWheels = 0;
+        StartCoroutine(Countdown(3));
     }
-
 
     void Update()
     {
+        
+    }
+
+    IEnumerator Countdown(int seconds)
+    {
+        int count = seconds;
+        while (count > 0)
+        {
+            // Update CountdownText
+            CountdownText.text = count.ToString();
+            yield return new WaitForSeconds(1);
+            count--;
+        }
+
+        // Countdown finished
+        CountdownText.text = "GO!";
+        // Unlock car
+        m_CarController.FullTorqueOverAllWheels = 2500;
+        yield return new WaitForSeconds(1);
+        CountdownText.text = "";
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        // UNLOCK CAR!
         
     }
 }
