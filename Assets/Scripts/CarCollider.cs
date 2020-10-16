73672664a683bd286c44474a02fc6aa87ceddfdf
Oriol Burgaya <oriolbv@gmail.com>
2020-10-16 12:18:30 +0200

    Circuit enter & exit implemented

---
+++ Assets/Scripts/CarCollider.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Vehicles.Car;
+
+public class CarCollider : MonoBehaviour
+{
+
+    public CarController m_CarController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer.Equals(9))
+        {
+            Debug.Log("ENTER CIRCUIT");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.layer.Equals(9))
+        {
+            Debug.Log("EXIT CIRCUIT");
+        }
+    }
+}
